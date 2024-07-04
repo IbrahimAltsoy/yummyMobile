712d19cee16560f357c87bdf89d425bf904c0703
Your Name <i.altsoy@gmail.com>
2024-07-04 13:33:45 +0300

    added storage

--- src/app/screens/homeScreeen/HomeScreen.js
+++ src/app/screens/homeScreeen/HomeScreen.js
@@ -1,10 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 
 function HomeScreen() {
+  const navigation = useNavigation();
+  const [accessToken, setAccessToken] = useState("");
+  const [expiresIn, setExpiresIn] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  async function handleTakeToken() {
+    const token = await AsyncStorage.getItem("accessToken");
+    setAccessToken(token);
+    const refreshToken = await AsyncStorage.getItem("refreshToken");
+    setRefreshToken(refreshToken);
+    const expiresIn = await AsyncStorage.getItem("expiresIn");
+    setExpiresIn(expiresIn);
+  }
+  function handleLogout() {
+    // setAccessToken("");
+    // setRefreshToken("");
+    // setExpiresIn("");
+    AsyncStorage.clear();
+
+    navigation.navigate("Yummy Application");
+  }
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Token Getir:" onPress={handleTakeToken} />
+      <Button title="Çıkış yap:" onPress={handleLogout} />
+      <Text>accessToken: {accessToken}</Text>
+      <Text>refreshToken: {refreshToken}</Text>
+      <Text>expiresIn: {expiresIn}</Text>
     </View>
   );
 }
--- src/app/screens/loginScreen/LoginScreen.js
+++ src/app/screens/loginScreen/LoginScreen.js
@@ -1,59 +1,96 @@
-import { Button, StyleSheet, Text, TextInput, View, Image } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Alert,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 import styles from "./LoginScreen.Style";
 import { LoginModel } from "./Login.Model";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function LoginScreen() {
   const navigation = useNavigation();
-  const loginModel = new LoginModel();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  loginModel.email = "ali@gmail.com";
-  loginModel.password = "123";
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "https://yummyapplication.com/api/Auth/authenticate",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      if (response.data.accessToken) {
+        // Access token ve diğer bilgileri AsyncStorage içinde saklıyoruz
+        await AsyncStorage.setItem("accessToken", response.data.accessToken);
+        await AsyncStorage.setItem("refreshToken", response.data.refreshToken);
+        await AsyncStorage.setItem(
+          "expiresIn",
+          response.data.expiresIn.toString()
+        );
+
+        navigation.navigate("anasayfa");
+        Alert.alert("Giriş yapıldı", response.data.accessToken);
+      } else {
+        Alert.alert("Hatalı bilgi girdiniz");
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Bir hata oluştu. Lütfen tekrar deneyin.");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Hoşgeldiniz</Text>
-      </View>
-      <Image
-        style={styles.image}
-        source={require("../../assets/images/yummyapp.jpg")}
-      />
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Username"
-          value={email}
-          onChangeText={setEmail}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Hoşgeldiniz</Text>
+        </View>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/yummyapp.jpg")}
         />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <Button
-          title="Giriş yap"
-          color="#FF6347"
-          onPress={() => {
-            if (email === "ali@gmail.com" && password === "123") {
-              navigation.navigate("anasayfa");
-            } else {
-              alert("Hatalı bilgi girdiniz");
-            }
-          }}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Üye ol"
-          onPress={() => navigation.navigate("register")}
-          color="#FF6347"
-        />
-      </View>
-    </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Username"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            autoCapitalize="none"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <Button title="Giriş yap" color="#FF6347" onPress={handleLogin} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Kaydol"
+            onPress={() => navigation.navigate("register")}
+            color="#FF6347"
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
